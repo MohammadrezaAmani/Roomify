fc4be9528153fc70c3c4547a21735a3d69d930f5
MohammadrezaAmani <more.amani@yahoo.com>
2023-07-31 16:10:25 +0330

    adding drf_yasg and swagger ui

--- Roomify/Roomify/urls.py
+++ Roomify/Roomify/urls.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Reservation API",
+        default_version='v1',
+        description="API endpoints for my application",
+        contact=openapi.Contact(email="contact@myapi.local"),
+    ),
+    public=True,
+)
+include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('listings/', include('listings.urls'), name='listings'),
     path('reservations/', include('reservations.urls'), name='reservations'),
     path('reports/', include('reports.urls'), name='reports'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
