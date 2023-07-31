8d2937e5248843a7fd83b7029eac9450b4e5c426
MohammadrezaAmani <more.amani@yahoo.com>
2023-07-31 16:20:19 +0330

    minor updates

--- Roomify/Roomify/settings.py
+++ Roomify/Roomify/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'Roomify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+        BASE_DIR / "templates" ,  
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
