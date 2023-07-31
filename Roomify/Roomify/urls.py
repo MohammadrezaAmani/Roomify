56b51fe16223f4478be95e1fa99d1667b7ea66fb
MohammadrezaAmani <more.amani@yahoo.com>
2023-07-31 15:27:15 +0330

    add urls of reservation app

--- Roomify/Roomify/urls.py
+++ Roomify/Roomify/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('listings/', include('listings.urls'), name='listings'),
+    path('reservations/', include('reservations.urls'), name='reservations'),
 ]
