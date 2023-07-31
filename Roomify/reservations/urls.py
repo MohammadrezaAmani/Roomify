bc87633a381207b5e1f8668558ee42cfbc623be9
MohammadrezaAmani <more.amani@yahoo.com>
2023-07-31 15:23:15 +0330

    adding checkAvailablity and ReservationFilter

--- Roomify/reservations/urls.py
+++ Roomify/reservations/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import (
     ReservationList,
     ReservationDetail,
+    ReservationFilter,
 )
 
 
 urlpatterns = [
     path('', ReservationList.as_view()),
     path('<int:pk>/', ReservationDetail.as_view()),
+    path('filter/', ReservationFilter),
 ]
--- Roomify/reservations/views.py
+++ Roomify/reservations/views.py
@@ -12,6 +12,9 @@ from .funcs import (
 from rest_framework import  generics
 from django.http import Http404
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 class ReservationList(generics.ListCreateAPIView):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
@@ -25,4 +28,17 @@ class ReservationDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
     
-# TODO: check availability
+# check availibility using pk and dates
+@api_view(['GET'])
+def ReservationFilter(request):
+    room = request.query_params.get('room', None)
+    start_date = request.query_params.get('start_date', None)
+    end_date = request.query_params.get('end_date', None)
+    if room is not None and start_date is not None and end_date is not None:
+        if is_free({'room': room, 'start_date': start_date, 'end_date': end_date}):
+            return Response({'is_free': True})
+        return Response({'is_free': False})
+    if room is not None and start_date is None and end_date is None:
+        queryset = Reservation.objects.filter(room=room)
+        serializer = ReservationSerializer(queryset, many=True)
+        return Response(serializer.data)
